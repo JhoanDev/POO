9c3e5a0d3d280effc8fc20f03cffbd8cacdaa9be
JhoanDev <jhoandev.log@gmail.com>
2023-12-02 09:51:27 -0300

    Feat: Q07

---
+++ Introducao/Q07.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Q07 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Double[] vet = new Double[3];
+        for (int i = 0; i < 3; i++){
+            System.out.printf("Insira o %d° número: ", i+1);
+            vet[i] = scan.nextDouble();
+        }
+        if (vet[0] <= vet[1] && vet[0] <= vet[2]) {
+            System.err.println("O menor número é: "+ vet[0]);
+        }
+        else if (vet[1] <= vet[0] && vet[1] <= vet[2]) {
+            System.err.println("O menor número é: "+ vet[1]);
+        }
+        else{
+            System.err.println("O menor número é: "+ vet[2]);
+        }
+        scan.close();
+    }
+}
