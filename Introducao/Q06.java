14ee2e41e75caa52a6056d6a32b2c225c76f83b3
JhoanDev <jhoandev.log@gmail.com>
2023-12-02 09:42:08 -0300

    refact: "scan"

--- Introducao/Q06.java
+++ Introducao/Q06.java
@@ -5,19 +5,20 @@ public class Q06 {
         Scanner scan = new Scanner(System.in);
         System.err.print("Insira os minutos totais: ");
         int mintotal = scan.nextInt();
+        scan.close();
         int dias = 0;
         int horas = 0;
         int minutos = mintotal;
         int i = 0;
-        for(i = minutos; i >= 1440; i -= 1440){
+        for (i = minutos; i >= 1440; i -= 1440) {
             dias++;
         }
         minutos = i;
-        for(i = minutos; i >= 60; i -= 60){
+        for (i = minutos; i >= 60; i -= 60) {
             horas++;
         }
         minutos = i;
-        System.out.printf("%d minutos = %d dias, %d horas e %d minutos.\n",mintotal,dias,horas,minutos);
-        scan.close();
+        System.out.printf("%d minutos = %d dias, %d horas e %d minutos.\n", mintotal, dias, horas, minutos);
+        
     }
 }
