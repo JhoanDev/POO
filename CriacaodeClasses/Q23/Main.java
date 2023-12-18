6a1a43a9d1153d42bc9394d2cfe2de9b57bbb708
JhoanDev <jhoandev.log@gmail.com>
2023-12-18 14:05:47 -0300

    Feat: init q23

---
+++ CriacaodeClasses/Q23/Main.java
@@ -0,0 +1,7 @@
+package CriacaodeClasses.Q23;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ CriacaodeClasses/Q23/Time.java
@@ -0,0 +1,12 @@
+package CriacaodeClasses.Q23;
+
+public class Time {
+    private String nome;
+    private int vitorias;
+    private int derrotas;
+    private int empates;
+    private int pontos;
+    private int saldo;
+
+    public
+}
