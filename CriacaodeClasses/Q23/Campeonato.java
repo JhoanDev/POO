b8d580a592b2eac58be8bb8f2493e2171b876114
JhoanDev <jhoandev.log@gmail.com>
2023-12-18 15:38:59 -0300

    feat: Q23

---
+++ CriacaodeClasses/Q23/Campeonato.java
@@ -0,0 +1,42 @@
+public class Campeonato {
+    public static void main(String[] args) {
+        Time flamengo = new Time("Flamengo");
+        Time palmeiras = new Time("Palmeiras");
+        Time vasco = new Time("Vasco");
+        Time cruzeiro = new Time("Cruzeiro");
+        
+        System.out.print("Partida 1\t");
+        flamengo.partida(palmeiras);
+        System.out.print("Partida 2\t");
+        flamengo.partida(vasco);
+        System.out.print("Partida 3\t");
+        flamengo.partida(cruzeiro);
+        
+        System.out.print("Partida 4\t");
+        palmeiras.partida(flamengo);
+        System.out.print("Partida 5\t");
+        palmeiras.partida(vasco);
+        System.out.print("Partida 6\t");
+        palmeiras.partida(cruzeiro);
+        
+        System.out.print("Partida 7\t");
+        vasco.partida(flamengo);
+        System.out.print("Partida 8\t");
+        vasco.partida(palmeiras);
+        System.out.print("Partida 9\t");
+        vasco.partida(cruzeiro);
+        
+        System.out.print("Partida 10\t");
+        cruzeiro.partida(flamengo);
+        System.out.print("Partida 11\t");
+        cruzeiro.partida(palmeiras);
+        System.out.print("Partida 12\t");
+        cruzeiro.partida(cruzeiro);
+
+        System.out.println("");
+        flamengo.exibirTime();
+        vasco.exibirTime();
+        cruzeiro.exibirTime();
+        palmeiras.exibirTime();
+    }
+}
--- CriacaodeClasses/Q23/Time.java
+++ CriacaodeClasses/Q23/Time.java
@@ -1,8 +1,49 @@
+import java.util.Random;
+
 public class Time {
-    private String nome;
-    private int vitorias;
-    private int derrotas;
-    private int empates;
-    private int pontos;
-    private int saldo;
+    String nome;
+    int vitorias;
+    int derrotas;
+    int empates;
+    int pontos;
+    int saldo;
+
+    Time(String nome) {
+        this.nome = nome;
+        this.vitorias = 0;
+        this.derrotas = 0;
+        this.empates = 0;
+        this.pontos = 0;
+        this.saldo = 0;
+    }
+
+    void partida(Time b) {
+        Random geradorAleatorio = new Random();
+        int golsA = geradorAleatorio.nextInt(6);
+        int golsB = geradorAleatorio.nextInt(6);
+        if (golsA > golsB) {
+            this.vitorias++;
+            this.pontos += 3;
+            b.derrotas++;
+            this.saldo += golsA - golsB;
+            b.saldo -= golsA - golsB;
+        } else if (golsA < golsB) {
+            this.derrotas++;
+            b.pontos += 3;
+            b.vitorias++;
+            this.saldo -= golsB - golsA;
+            b.saldo += golsB - golsA;
+        } else {
+            this.empates++;
+            this.pontos++;
+            b.empates++;
+            b.pontos++;
+        }
+        System.out.printf("%s %dx%d %s\n", this.nome, golsA, golsB, b.nome);
+    }
+
+    void exibirTime() {
+        System.out.printf("Nome:%s\tPontos:%d\tVitórias:%d\tEmpates:%d\tSaldo:%d\n", nome, pontos, vitorias, empates,
+                saldo);
+    }
 }
