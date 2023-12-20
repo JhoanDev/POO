9548927d0da3aadfe2e1c72f4df9cd0a54545814
JhoanDev <jhoandev.log@gmail.com>
2023-12-20 17:42:34 -0300

    feat: Circulo

---
+++ EmSala/Classes/Circulo/DemoCirculo.java
@@ -0,0 +1,7 @@
+public class DemoCirculo {
+    public static void main(String[] args) {
+        Circulo circ1 = new Circulo(2.5);
+        System.out.println("Perimetro: " + circ1.perimetro());
+        System.out.println(Circulo.pi);
+    }
+}
