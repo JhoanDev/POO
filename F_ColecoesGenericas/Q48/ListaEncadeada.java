b25907e297f20c3fc7d114a828476cf32171e63d
JhoanDev <jhoandev.log@gmail.com>
2024-03-20 20:37:45 -0300

    feat: Q48

---
+++ F_ColecoesGenericas/Q48/ListaEncadeada.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ListaEncadeada {
+    public static void main(String[] args) {
+        LinkedList <Character> lista = new LinkedList<>(Arrays.asList('A','B','C','D','E','F','G','H','I','J'));
+        LinkedList <Character> copia = new LinkedList<>();
+        ListIterator <Character> itr = lista.listIterator();
+        while (itr.hasNext())
+            itr.next();
+        while (itr.hasPrevious()) 
+            copia.add(itr.previous());
+        System.out.println("Lista inicial: " + lista);
+        System.out.println("Copia invertida: " + copia);
+    }
+}
