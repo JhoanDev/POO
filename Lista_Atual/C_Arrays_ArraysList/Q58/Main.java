d27811a7593b248a7932732bb314de5d64c33838
JhoanDev <jhoandev.log@gmail.com>
2025-05-28 10:52:02 -0300

    feat: Q58

---
+++ Lista_Atual/C_Arrays_ArraysList/Q58/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] A = { 12, 25, 16, 11, 9, 31, 23, 27, 22 };
+        System.out.println(A[A[2] - A[4]] - A[6]);
+    }
+}
