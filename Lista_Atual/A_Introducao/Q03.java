6d88294ecc82e47a42cfd930ab2082fe3cf4a037
JhoanDev <jhoandev.log@gmail.com>
2025-08-26 14:12:13 -0300

    feat: Q3

--- Lista_Atual/A_Introducao/Q03.java
+++ Lista_Atual/A_Introducao/Q03.java
@@ -1,5 +1,23 @@
 package Lista_Atual.A_Introducao;
 
+import java.util.Scanner;
+import java.lang.Math;
+
 public class Q03 {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Insira um angulo em graus: ");
+        double graus = Double.parseDouble(scan.nextLine());
+        double rad = Math.toRadians(graus);
+        double seno = Math.sin(rad);
+        double cosseno = Math.cos(rad);
+        double tangente = seno / cosseno;
+        double cossecante = 1 / seno;
+        double secante = 1 / cosseno;
+        double cotangente = 1 / tangente;
+        System.out.printf(
+                "Graus: %g°\nRadianos: %g\nSeno:%g\nCosseno: %g\nTangente: %g\nCossecante: %g\nSecante: %g\nCotangente: %g\n",
+                graus, rad, seno, cosseno, tangente, cossecante, secante, cotangente);
+        scan.close();
+    }
 }
